d6c7bffc8635137296ffab0238a21df80b1c6587
장홍기 <power374@naver.com>
2024-05-17 14:36:40 +0900

    api get post patch delete learning

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Param, Post, Delete, Put, Patch, Body} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+
+@Controller('movies')
+export class MoviesController {
+
+    constructor(private readonly moviesService: MoviesService) {}
+
+    @Get()
+    getAll(): Movie[]{
+        return this.moviesService.getAll();
+    }
+
+    @Get('/:id')
+    getOne(@Param('id') movieid:number) : Movie{
+        return this.moviesService.getOne(movieid);
+    }
+
+    @Post()
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') movieId:number){
+        return this.moviesService.deleteOne(movieId);
+    }
+
+    @Patch('/:id')
+    patcy(@Param('id') movieId:number, @Body() updateData : UpdateMovieDto){
+        return this.moviesService.update(movieId, updateData);
+    }   
+}
